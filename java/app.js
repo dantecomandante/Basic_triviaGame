c48c71a786dc4b7657f40804bc9e615fed334be1
Dante Perez <red.dante888@gmail.com>
2019-08-12 03:08:59 -0700

    commit

---
+++ java/app.js
@@ -0,0 +1,29 @@
+let questions = [{
+    question: "In a roundabout, you must drive?",
+    answers: ["A counterclockwise direction", "Clockwise direction", "The direction of oncoming vehicles"],
+    correctAnswer: "A counterclockwise direction",
+  }, {
+    question: "To avoid tailgating, one should follow the",
+    answers: ["3 second rule", "4 second rule", "2 second rule"],
+    correctAnswer: "3 second rule",
+  }, {
+    question: "A(n) __________ sign on a truck means that the truck's load is potentially dangerous.",
+    answers: ["diamond shape", "octagonal", "rectangular white"],
+    correctAnswer: "diamond shape",
+  }, {
+    question: "If your parked car rolls away and hits another unattended vehicle, you should",
+    answers: ["Drive away fast", "Make a report to the cops", "leave a sorry note on the window"],
+    correctAnswer: "Make a report to the cops",
+  }, {
+    question: "A ___ painted curb is designated for disabled persons who display their placards.",
+    answers: ["White", "Blue", "Green"],
+    correctAnswer: "Blue",
+  }, {
+    question: "You must turn on your headlights __________ after sunset.",
+    answers: ["20 minutes", "immediatly", "30 minutes"],
+    correctAnswer: "30 minutes",
+  }, {
+    question: "When you want to enter a freeway, you should",
+    answers: ["stop before merging onto the freeway traffic", "stop and yield to the freeway traffic", "enter the freeway at or near the speed of the traffic"],
+    correctAnswer: "enter the freeway at or near the speed of the traffic",
+  }];
